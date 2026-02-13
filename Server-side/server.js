712b7079e8bbf591d6ede8c557da92a1988a22a5
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2026-02-13 19:24:38 +0530

    lwjfgsh

--- Server-side/server.js
+++ Server-side/server.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser")
 
-require("dotenv").config()
+require("dotenv").config();
 
-const globalErrorController = require("./Controllers/errorController")
+const globalErrorController = require("./Controllers/errorController");
 
 const app = express();
 
@@ -28,11 +28,11 @@ app.get("/",(req,res)=>{
     res.status(200).json({
         message : "this is the home page"
     })
-})
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const connectToDB = require("./DataBase/db")
+const connectToDB = require("./DataBase/db");
 
 connectToDB();
 
