7a5fab8a9d96066107a55e694e3796070c68c735
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2026-02-19 14:14:22 +0530

    basic utils folder added

---
+++ Server-side/Controllers/auth-controller.js
@@ -0,0 +1,1 @@
+const catchAsync = require("../Utils/catchAsync");
---
+++ Server-side/Utils/send-mail.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer')
+
+const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+        service : 'gmail',
+        auth : {
+            user : process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    })
+
+    const mailOptions = {
+        from: `"Your App Name" <${process.env.EMAIL}>`, // adds a friendly sender name
+        to: options.email,
+        subject: options.subject,
+        text: options.text || "Please view this email in HTML format.", // fallback text
+        html: options.html, 
+    }
+
+    await transporter.sendMail(mailOptions)
+}
+
+module.exports = sendEmail
