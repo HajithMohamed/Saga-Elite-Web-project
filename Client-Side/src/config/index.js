e0be3fb342bb79c6860a8041fed86a17ce2754c8
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2026-03-10 17:58:02 +0530

    added the missing login form control

--- Client-Side/src/config/index.js
+++ Client-Side/src/config/index.js
@@ -1,7 +1,25 @@
 import { Component } from "lucide-react";
 
-
+export const loginFormControl = [
+  {
+    name: "email",
+    type: "email",
+    placeholder: "exampl@gmail.com",
+    id: "email",
+    label: "Email",
+    componentType : "INPUT"
+  },
+  {
+    name: "password",
+    type: "password",
+    placeholder: "ABcd@12",
+    id: "password",
+    label: "Password",
+    componentType : "INPUT"
+  },
+];
 export const registerFormControl = [
+  
   {
     name: "email",
     type: "email",
