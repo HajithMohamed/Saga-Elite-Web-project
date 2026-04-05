5365d4b56c971eab2e5921b649baeb07555b1732
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2026-04-05 17:59:49 +0530

    some modifications

--- Server-side/Config/cors-config.js
+++ Server-side/Config/cors-config.js
@@ -1,5 +1,5 @@
 const cors = require("cors")
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://localhost:5175"];
 
 const configureCors = ()=>{
     return cors({
