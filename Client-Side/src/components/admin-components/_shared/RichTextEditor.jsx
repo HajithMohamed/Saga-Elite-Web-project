600bfe37a7faeae3ebfe0b235ca098642bf0d86e
AkMJafran <mohamedjafran300@gmail.com>
2026-05-11 21:31:23 +0530

    policies in admin  fixed

--- Client-Side/src/components/admin-components/_shared/RichTextEditor.jsx
+++ Client-Side/src/components/admin-components/_shared/RichTextEditor.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import Link from "@tiptap/extension-link";
-import Underline from "@tiptap/extension-underline";
-import Table from "@tiptap/extension-table";
-import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
+import { StarterKit } from "@tiptap/starter-kit";
+import { Link } from "@tiptap/extension-link";
+import { Underline } from "@tiptap/extension-underline";
+import { Table } from "@tiptap/extension-table";
+import { TableRow } from "@tiptap/extension-table-row";
+import { TableCell } from "@tiptap/extension-table-cell";
+import { TableHeader } from "@tiptap/extension-table-header";
 import {
   Bold,
   Italic,
